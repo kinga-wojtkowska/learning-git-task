725c14d0eb39f587a3616b8c572ac5ef1be08662
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-07-16 23:34:28 +0200

    Add print text from dict

--- task_1.py
+++ task_1.py
@@ -2,3 +2,6 @@ shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+for k, v in shopping_list.items():
+    print(f"Idę do: {k.capitalize()}, kupuję tu następujące rzeczy:", [i.capitalize() for i in v], ".")
