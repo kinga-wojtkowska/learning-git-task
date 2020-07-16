9483f6787e4264ff0020b68a51cb84af4ea3cacb
Kinga Wojtkowska <kinga.wolynska@gmail.com>
2020-07-16 23:36:07 +0200

    Add dict values length

--- task_1.py
+++ task_1.py
@@ -5,3 +5,8 @@ shopping_list = {
 
 for k, v in shopping_list.items():
     print(f"Idę do: {k.capitalize()}, kupuję tu następujące rzeczy:", [i.capitalize() for i in v], ".")
+
+length = 0
+for k, v in shopping_list.items():
+    length = length + len(shopping_list[k])
+print("W sumie kupuję", length, "produktów")
